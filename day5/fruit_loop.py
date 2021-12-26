81a762827f1275474a639e10d0b74abc66df4401
Roberth Lombardi <roberth.lombardi@molnbolaget.com>
2021-12-26 18:03:02 +0100

    asd

---
+++ day5/fruit_loop.py
@@ -0,0 +1,4 @@
+fruits = ["Apple", "Peach", "Pear"]
+
+for each in fruits:
+    print(each)
---
+++ day5/maffs.py
@@ -0,0 +1,6 @@
+total_maffs = 0
+
+for x in range(0, 101, 2):
+    total_maffs += x
+
+print(total_maffs)
